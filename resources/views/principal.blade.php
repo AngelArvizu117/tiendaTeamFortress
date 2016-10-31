f7afd5140db510cfbc694afcb2b1861dd305248e
AngelArvizu117 <ing.angel.arvizu@gmail.com>
2016-10-31 13:10:42 -0600

    Agregacion de imagenes

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -85,16 +85,28 @@
                                 <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
                                 <li data-target="#carousel-example-generic" data-slide-to="1"></li>
                                 <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="5"></li>
                             </ol>
                             <div class="carousel-inner">
                                 <div class="item active">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                    <img class="slide-image" src="{{asset ("/img/teamfortress.jpg")}}" alt="">
                                 </div>
                                 <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                    <img class="slide-image" src="{{asset ("/img/mapa1.jpg")}}" alt="">
                                 </div>
                                 <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                    <img class="slide-image" src="{{asset ("/img/mapa2.jpg")}}" alt="">
+                                </div>
+                                <div class="item">
+                                    <img class="slide-image" src="{{asset ("/img/mapa3.jpg")}}" alt="">
+                                </div>
+                                <div class="item">
+                                    <img class="slide-image" src="{{asset ("/img/mapa4.jpg")}}" alt="">
+                                </div>
+                                <div class="item">
+                                    <img class="slide-image" src="{{asset ("/img/mapa5.jpg")}}" alt="">
                                 </div>
                             </div>
                             <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
@@ -112,7 +124,7 @@
 
                     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
+                            <img src="{{asset ("/img/arma5.jpg")}}" alt="">
                             <div class="caption">
                                 <h4 class="pull-right">$24.99</h4>
                                 <h4><a href="#">First Product</a>
@@ -134,10 +146,10 @@
 
                     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
+                            <img src="{{asset ("/img/arma1.png")}}" alt="">
                             <div class="caption">
                                 <h4 class="pull-right">$64.99</h4>
-                                <h4><a href="#">Second Product</a>
+                                <h4><a href="#">Arma Secundaria</a>
                                 </h4>
                                 <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                             </div>
@@ -156,7 +168,7 @@
 
                     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
+                            <img src="{{asset ("/img/arma2.png")}}" alt="">
                             <div class="caption">
                                 <h4 class="pull-right">$74.99</h4>
                                 <h4><a href="#">Third Product</a>
@@ -178,7 +190,7 @@
 
                     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
+                            <img src="{{asset ("/img/arma3.png")}}" alt="">
                             <div class="caption">
                                 <h4 class="pull-right">$84.99</h4>
                                 <h4><a href="#">Fourth Product</a>
@@ -200,7 +212,7 @@
 
                     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
+                            <img src="{{asset ("/img/arma4.png")}}" alt="">
                             <div class="caption">
                                 <h4 class="pull-right">$94.99</h4>
                                 <h4><a href="#">Fifth Product</a>
@@ -224,7 +236,7 @@
                         <h4><a href="#">Te gusto la tienda?</a>
                         </h4>
                         <p>Si te ha gustado esta pagina web, entonces puedes ver <a target="_blank" href="https://www.youtube.com/watch?v=a8ZpAf_tNh0&list=PL3ZhWMazGi9IYymniZgqwnYuPFDvaEHJb">este tutorial</a> que te dira paso a paso como instalar y utilizar laravel 5.3 para que puedas realizar tu tienda online!</p>
-                        <a class="btn btn-primary" target="_blank" href="https://github.com/AngelArvizu117/ProyectoTiendaEnLinea">Repositorio</a>
+                        <a class="btn btn-primary" target="_blank" href="https://github.com/AngelArvizu117/tiendaTeamFortress">Repositorio</a>
                     </div>
 
                 </div>
