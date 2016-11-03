0c2c951ceb7f96568a11884e7596df41e4ee059e
AngelArvizu117 <ing.angel.arvizu@gmail.com>
2016-11-03 16:10:12 -0600

    Base de datos actualizada

--- tiendaonline.sql
+++ tiendaonline.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-11-2016 a las 17:32:06
+-- Tiempo de generación: 03-11-2016 a las 23:06:09
 -- Versión del servidor: 10.1.13-MariaDB
 -- Versión de PHP: 5.6.21
 
@@ -19,6 +19,43 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `tiendaonline`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id_c` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compras`
+--
+
+CREATE TABLE `compras` (
+  `id_com` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compras_productos`
+--
+
+CREATE TABLE `compras_productos` (
+  `id_cp` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_compra` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `importe` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -55,6 +92,37 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_p` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `precio` float NOT NULL,
+  `comentarios` text NOT NULL,
+  `evaluacion` int(5) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `imagen` varchar(200) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos_categorias`
+--
+
+CREATE TABLE `productos_categorias` (
+  `id_pc` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `users`
 --
 
@@ -76,34 +144,27 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `creat
 (1, 'joel', 'joel@hotmail.com', '$2y$10$Bo9XNgiYNwIcwW4pXfDAOuegxzYZMa23sC99aHRuAOy7iMUPhubPq', '9f0hISOdZArqgeyJ5Cahi3D5OdEokl9tDov0FCkdTQMNw2mvdWAXUSrwOetd', '2016-11-01 03:04:13', '2016-11-02 01:53:21'),
 (2, 'angel', 'angel@gmail.com', '$2y$10$mHoTmlgbEad3GG3Yd4FSzOM1NaEDCAjBK3tb.WFjrpXDUcpSQQLnq', 'EPIxFUtMVG8WnaHI5blT5X1pQ10aUkzYTwgJ9A1ISTvImexSPWpn3drVBnfz', '2016-11-01 03:26:00', '2016-11-02 01:53:39');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `edad` int(11) NOT NULL,
-  `direccion` varchar(200) NOT NULL,
-  `correo` varchar(50) NOT NULL,
-  `telefono` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nombre`, `edad`, `direccion`, `correo`, `telefono`, `created_at`, `updated_at`) VALUES
-(1, 'Angel Arvizu', 23, 'Enrique Segoviano #111', 'ing.angel.arvizu@gmail.com', 66, '2016-10-31 13:19:14', '2016-10-31 13:19:14'),
-(2, 'Manuel Montoya', 24, 'Enrique Segoviano #111', 'ing.manuel.montoya@gmail.com', 66713456, '2016-10-31 22:23:00', '2016-10-31 22:23:00');
-
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id_c`);
+
+--
+-- Indices de la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`id_com`);
+
+--
+-- Indices de la tabla `compras_productos`
+--
+ALTER TABLE `compras_productos`
+  ADD PRIMARY KEY (`id_cp`);
 
 --
 -- Indices de la tabla `migrations`
@@ -119,6 +180,18 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_token_index` (`token`);
 
 --
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_p`);
+
+--
+-- Indices de la tabla `productos_categorias`
+--
+ALTER TABLE `productos_categorias`
+  ADD PRIMARY KEY (`id_pc`);
+
+--
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
@@ -126,30 +199,44 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id_c` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `compras`
+--
+ALTER TABLE `compras`
+  MODIFY `id_com` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `compras_productos`
+--
+ALTER TABLE `compras_productos`
+  MODIFY `id_cp` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `productos_categorias`
+--
+ALTER TABLE `productos_categorias`
+  MODIFY `id_pc` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
